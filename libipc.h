513521bd33f38619f30209eb76b1a427fc2b2f9c
LampCarpet <supersomeone71@hotmail.com>
2014-11-20 22:35:15 +0800

    small changes

--- libipc.h
+++ libipc.h
@@ -12,10 +12,22 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+=============================================================================
+Interprocess communication library, (libipc)
 
+Filename:
+ 		libipc.h
 
+Version:
+ 		0.0.1-dev (unstable)
 
+Referred to as:
+		"libipc-header", "libipc-api", "api"
 
+Description:
+		
+
+File debug refs:
 
 */
 #pragma once
